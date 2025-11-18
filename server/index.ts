a5269e3aebe5314ccffb996d44266eb66aee9dc3
marnand <marnandf@gmail.com>
2025-11-17 22:13:04 -0300

    teste guthubpages

--- server/index.ts
+++ server/index.ts
@@ -57,7 +57,7 @@ app.use((req, res, next) => {
     throw err;
   });
 
-  // importantly only setup vite in development and after
+  // importantly only setup vite in development and after 
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
   if (app.get("env") === "development") {
